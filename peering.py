98adaea1fcd00021ac105aeb576daeae49f60b80
frederic-loui <frederic.loui@gmail.com>
2020-06-15 12:12:40 +0200

    early Cisco IOSXR and Juniper JUNOS parser

--- peering.py
+++ peering.py
@@ -19,7 +19,7 @@
 ###############################################################################
 
 from threading import Thread
-import argparse, glob,os, sys,logging,  re, linecache, json
+import argparse, glob,os, sys,logging,  re, linecache, json,ipaddress
 from time import sleep
 
 from ciscoconfparse import CiscoConfParse
@@ -96,29 +96,44 @@ class PPM(Thread):
             self.getAsnPeerListFromIosXR(cfg)
 
     def getAsnPeerListFromJunos(self, cfg):
-        parse = CiscoConfParse("%s" % cfg, syntax='junos', comment='#!')
+        parse = CiscoConfParse("%s" % cfg, syntax='junos', comment='#')
+        intf_data = self.getInterfaceListFromJunos(parse,cfg)
+        #logger.warning("INTF_DATA:%s" % intf_data)
         peer_list_obj=parse.find_objects(" peer-as")
         peer_list=[]
         logger.warning("CFG_JUNOS: %s" % cfg)
         for peer in peer_list_obj:
             if not peer.text in peer_list:
                 peer_list.append(peer.text)
+        #logger.warning("PEER_LIST:%s" % peer_list)
         for peer in peer_list:
             PEER = {}
             PEER["asn"] = ""
             PEER["remote_neighbors"] = []
+            peer_ip=""
             remote_neighbors = parse.find_parents_w_child(r"  neighbor ", peer)
-
+            #intf_data = {}
             for neighbor in remote_neighbors:
-                PEER["remote_neighbors"].append(re.sub(r"\s+|neighbor", "", neighbor))
+                peer_ip=re.sub(r"\s+|neighbor", "", neighbor)
+                peer_info = self.getPeerInterfaceSubnetJunos(intf_data,peer_ip)
+                #logger.warning("PEER_INFO_1:%s" % peer_info)
+                if intf_data is not None:
+                    PEER["remote_neighbors"].append(peer_info)
 
             if len(PEER["remote_neighbors"]) == 0:
+            #if PEER["remote_neighbors"] is None:
                 peer_as_parents=parse.find_parents_w_child(r"group", peer)
                 for parent in peer_as_parents:
                     remote_neighbors = parse.find_children_w_parents(parent, r"  neighbor ")
                     for neighbor in remote_neighbors:
-                        PEER["remote_neighbors"].append(re.sub(r"\s+|neighbor", "", neighbor))
+                        peer_ip=re.sub(r"\s+|neighbor", "", neighbor)
+                        peer_info = self.getPeerInterfaceSubnetJunos(intf_data,peer_ip)
+                        #logger.warning("PEER_INFO_2:%s" % peer_info)
+                        if peer_info is not None:
+                        #PEER["remote_neighbors"].append(re.sub(r"\s+|neighbor", "", neighbor))
+                            PEER["remote_neighbors"].append(peer_info)
 
+            #if not PEER["remote_neighbors"] is None :
             if not len(PEER["remote_neighbors"]) == 0:
                 PEER["asn"] = re.sub(r"\s+|peer-as","",peer)
                 ACTIVE_PEERS.append(PEER)
@@ -126,9 +141,30 @@ class PPM(Thread):
         logger.warning("%s" % (ACTIVE_PEERS))
         with open("%s/%s.json" % (self.json_dir,re.sub(r".cfg$","",cfg)), 'w') as outjsonfile:
             json.dump(ACTIVE_PEERS, outjsonfile)
+            ACTIVE_PEERS.clear()
+
+    def getInterfaceListFromIosXR(self,parse,cfg):
+        branchspec = (r'^interface', r'address')
+        branches = parse.find_object_branches(branchspec=branchspec)
+        out={}
+        for branch in branches:
+            intf =""
+            local_ip =""
+            for elt in branch:
+                if elt is not None:
+                    if re.search(r"^interface", elt.text):
+                        intf=re.sub(r"\s+|interface","",elt.text)
+                    if re.search(r"address", elt.text):
+                        local_ip=re.sub(r"^\s+|address|ipv4|ipv6","",elt.text).strip()
+                        local_ip=re.sub(r"\s","/",local_ip)
+                        ipaddress.ip_network(local_ip,strict=False)
+                        out["%s" % local_ip]="%s" % intf
+        return out
+
 
     def getAsnPeerListFromIosXR(self,cfg):
-        parse = CiscoConfParse("%s" % cfg, syntax='ios', comment='!')
+        parse = CiscoConfParse("%s" % cfg, syntax='ios', comment='!',factory=True)
+        intf_data=self.getInterfaceListFromIosXR(parse ,cfg)
         peer_list_obj=parse.find_objects("^  remote-as")
         peer_list=[]
         logger.warning("CFG_IOSXR: %s" % cfg)
@@ -142,19 +178,101 @@ class PPM(Thread):
             PEER = {}
             PEER["asn"] = ""
             PEER["remote_neighbors"] = []
+            peer_ip = ""
             remote_neighbors = parse.find_parents_w_child(r"^ neighbor ", peer)
             logger.warning("REMOTE_NEIGHBORS=%s" % remote_neighbors)
 
             for neighbor in remote_neighbors:
-                PEER["remote_neighbors"].append(re.sub(r"\s+|neighbor", "", neighbor))
+                peer_ip=re.sub(r"\s+|neighbor", "", neighbor)
+                peer_info = self.getPeerInterfaceSubnetIosXR(intf_data,peer_ip)
+                if peer_info is not None:
+                    PEER["remote_neighbors"].append(peer_info)
 
-            if not len(PEER["remote_neighbors"]) == 0:
-                PEER["asn"] = re.sub(r"\s+|remote-as","",peer)
+            if not len(PEER["remote_neighbors"])==0:
+                PEER["asn"] = re.sub("\s+|remote-as","",peer )
                 ACTIVE_PEERS.append(PEER)
 
         logger.warning("%s" % (ACTIVE_PEERS))
         with open("%s/%s.json" % (self.json_dir,re.sub(r".cfg$","",cfg)), 'w') as outjsonfile:
             json.dump(ACTIVE_PEERS, outjsonfile)
+            ACTIVE_PEERS.clear()
+
+
+    def getPeerInterfaceSubnetIosXR(self,subnets_list,peer_ip):
+        out={}
+        for subnet in subnets_list.keys():
+            try:
+                intf_ip_subnet = ipaddress.ip_network(subnet,False)
+                local_ip = ipaddress.ip_address(peer_ip)
+                if local_ip in intf_ip_subnet:
+                    #ip_local=re.sub(r"%s"%intf_ip,"",subnet)
+                    ip_network=intf_ip_subnet.network_address
+                    ip_netmask=intf_ip_subnet.netmask
+                    ip_cidr=intf_ip_subnet.prefixlen
+                    local_intf=subnets_list[subnet]
+                    #logger.warning("LOCAL_IP:%s" % (ip_local))
+                    #logger.warning("IP_NETWORK:%s" % (ip_network))
+                    #logger.warning("IP_NETMASK:%s" % (ip_netmask))
+                    #logger.warning("IP_CIDR:%s" % (ip_cidr))
+                    #logger.warning("LOCAL_INTF: %s" % (local_intf) )
+                    out["local_ip"]="%s" % local_ip
+                    out["peer_ip"]="%s" % peer_ip
+                    out["subnet"]="%s" % ip_network
+                    out["cidr"]="%s" % ip_cidr
+                    out["interface"]="%s" % local_intf
+                    return out
+            except (ValueError, TypeError):
+                logger.warning("%s is not a valid IPv4/IPv6 network:" % (ipaddr))
+
+    def getInterfaceListFromJunos(self,parse,cfg):
+        parse_intf=CiscoConfParse(parse.find_all_children("^interface"))
+        branchspec = (r'^interface', r'^    [a-z]{2}', r'unit', r'family inet', r'address')
+        branches = parse_intf.find_object_branches(branchspec=branchspec)
+        subnets_list = {}
+        for branch in branches:
+            intf =""
+            unit =""
+            local_ip =""
+            intf_data =""
+            for elt in branch:
+               if elt is not None:
+                  if not re.search(r"inactive",elt.text):
+                     if re.search(r"^    [a-z]{2}",elt.text):
+                        intf=re.sub(r"\s+|{","",elt.text)
+                     if re.search(r"unit",elt.text):
+                        unit=re.sub(r"\s+|unit|{","",elt.text)
+                     if re.search(r"address",elt.text):
+                        local_ip=re.sub(r"\s+|address|{","",elt.text)
+                        intf_data = "{interface:%s.%s,local_ip:%s}" % (intf,unit,local_ip)
+                        subnets_list[local_ip]="%s.%s" % (intf,unit)
+        return subnets_list
+
+    def getPeerInterfaceSubnetJunos(self,subnets_list,peer_ip):
+        #{local_ip:1.1.1.1,ip_network:1.1.1.0,ip_cidr:24,ip_intf:xe-0/0/0.0}
+        out={}
+        for subnet in subnets_list.keys():
+            try:
+                intf_ip_subnet = ipaddress.ip_network(subnet,False)
+                intf_ip = ipaddress.ip_address(peer_ip)
+                if intf_ip in intf_ip_subnet:
+                    ip_local=re.sub(r"\s|/[0-9]+$","",subnet)
+                    ip_network=intf_ip_subnet.network_address
+                    ip_netmask=intf_ip_subnet.netmask
+                    ip_cidr=intf_ip_subnet.prefixlen
+                    local_intf=subnets_list[subnet]
+                    #logger.warning("LOCAL_IP:%s" % (ip_local))
+                    #logger.warning("IP_NETWORK:%s" % (ip_network))
+                    #logger.warning("IP_NETMASK:%s" % (ip_netmask))
+                    #logger.warning("IP_CIDR:%s" % (ip_cidr))
+                    #logger.warning("LOCAL_INTF: %s" % (local_intf) )
+                    out["local_ip"]="%s" % ip_local
+                    out["peer_ip"]="%s" % peer_ip
+                    out["subnet"]="%s" % ip_network
+                    out["cidr"]="%s" % ip_cidr
+                    out["interface"]="%s" % local_intf
+                    return out
+            except (ValueError, TypeError):
+                logger.warning("%s is not a valid IPv4/IPv6 network:" % (ipaddr))
 
 def str2bool(v):
     if isinstance(v, bool):
@@ -179,7 +297,7 @@ if __name__ == "__main__":
 
     parser.add_argument(
         "--oxidized-address",
-        help="freerouter address",
+        help="oxidized address",
         type=str,
         action="store",
         required=False,
@@ -187,7 +305,7 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--oxidized-port",
-        help="freerouter port",
+        help="oxidized port",
         type=int,
         action="store",
         required=False,
